5db3a665c1667b6041628c63a870bcf4db23277e
Sahil Chopra <67851308+sash9696@users.noreply.github.com>
2024-07-18 20:35:49 +0530

    Add files via upload

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -16,7 +16,7 @@ function Auth() {
       // api request to login or register
 
       const { data } = await axios.post(
-        `http://localhost:3001/api/users${isRegister ? "" : "/login"}`,
+        `https://blogging-backend-k6f2.onrender.com/api/users${isRegister ? "" : "/login"}`,
         { user: values }
       );
 
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -25,7 +25,7 @@ function Settings() {
 
     async function onSubmit(values, {setErrors}){
         try {
-            const {data} = await axios.put(`http://localhost:3001/api/user`, {user:values});
+            const {data} = await axios.put(`https://blogging-backend-k6f2.onrender.com/api/user`, {user:values});
 
             const updatedUsername = data?.user?.username;
 
